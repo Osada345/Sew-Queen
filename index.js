b9fa539b96b2767990250043b18b23f57391c5dd
Osada345 <94613106+Osada345@users.noreply.github.com>
2022-09-27 17:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-const makeWASocket = require("@adiwajshing/baileys").default
-const qrcode = require("qrcode-terminal")
-const fs = require('fs')
-const pino = require('pino')
-const { delay , useSingleFileAuthState } = require("@adiwajshing/baileys")
-const { state, saveState } = useSingleFileAuthState('./sew-queen.tset1.json')
 
-  function qr() {
-//------------------------------------------------------
-    let version = [3,3234,9]
-    const session = makeWASocket({
-        logger: pino({ level: 'silent' }),
-        printQRInTerminal: true,
-        browser: ['TEST-V1','opera','1.0.0'],
-        auth: state,
-        version
-    })
-//------------------------------------------------------
-    session.ev.on("connection.update",async  (s) => {
-        const { connection, lastDisconnect } = s
-        if (connection == "open") {
-            await delay(1000 * 10)
-            process.exit(0)
-        }
-        if (
-            connection === "close" &&
-            lastDisconnect &&
-            lastDisconnect.error &&
-            lastDisconnect.error.output.statusCode != 401
-        ) {
-            qr()
-        }
-    })
-    session.ev.on('creds.update', saveState)
-    session.ev.on("messages.upsert",  () => { })
+
+
+return
+const bash = require("@ravindu01manoj/bash-js")
+const startFileWithNode = require("../indexstart.js")
+
+async function start() {
+	return await startFileWithNode(bash,"node manoj.js")
 }
-qr()
+start()
